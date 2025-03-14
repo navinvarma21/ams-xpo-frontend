0f2fe769d812ba1128c7d730d4f358f6a2c1580b
navinvarma2002 <navinvarma.a2002@gmail.com>
2025-03-14 15:04:39 +0000

    9th commit

--- src/Component/pages/AdminDashboard/Mainmenu.jsx
+++ src/Component/pages/AdminDashboard/Mainmenu.jsx
@@ -5,8 +5,29 @@ export default function Mainmenu() {
     <div className="Mainmenu-container">
       <h1>Admin Dashboard</h1>
       <div className="Mainmenu-container-details">
-        <div className="Mainmenu-container-details-1">1</div>
-        <div className="Mainmenu-container-details-2">2</div>
+        <div className="Mainmenu-container-details-1">
+
+          <div className="report">
+            <p className="Mainmenu-container-details-1-p">report1</p>
+          </div>
+
+          <div className="report">
+            <p className="Mainmenu-container-details-1-p">report2</p>
+          </div>
+
+        </div>
+
+        <div className="Mainmenu-container-details-2">
+
+          <div className="report">
+            <p className="Mainmenu-container-details-1-p">report3</p>
+          </div>
+
+          <div className="report">
+            <p className="Mainmenu-container-details-1-p">report4</p>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
