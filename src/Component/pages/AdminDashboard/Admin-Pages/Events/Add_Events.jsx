3dfc1f8e6049db676a605ec0d06f0c41b77baec5
navinvarma2002 <navinvarma.a2002@gmail.com>
2025-04-04 14:22:01 +0100

    48th commit

--- src/Component/pages/AdminDashboard/Admin-Pages/Events/Add_Events.jsx
+++ src/Component/pages/AdminDashboard/Admin-Pages/Events/Add_Events.jsx
@@ -1,171 +1,228 @@
-
-import React, { useState } from 'react'
+import React from "react";
 
 export default function Add_Events() {
-  const [step, setStep] = useState(1)
-
-  const nextStep = () => {
-    setStep(step + 1)
-  }
-
-  const prevStep = () => {
-    setStep(step - 1)
-  }
-
-  const renderFormSection = () => {
-    switch (step) {
-      case 1:
-        return (
-          <div className="section">
-            <h3>Basic Details:</h3>
-            <label htmlFor="eventName">Event Name:</label>
-            <input
-              type="text"
-              id="eventName"
-              name="eventName"
-              placeholder="Enter Event Name"
-            />
-
-            <label htmlFor="eventDescription">Event Description:</label>
-            <input
-              type="text"
-              id="eventDescription"
-              name="eventDescription"
-              placeholder="Enter Description"
-            />
-
-            <label htmlFor="eventCategory">Event Category:</label>
-            <select id="eventCategory" name="eventCategory">
-              <option>Select Event Category</option>
-              {/* Add options here */}
-            </select>
-
-            <label htmlFor="eventTags">Event Tags:</label>
-            <select id="eventTags" name="eventTags">
-              <option>Select Event Tags</option>
-              {/* Add options here */}
-            </select>
-          </div>
-        )
-      case 2:
-        return (
-          <div className="section">
-            <h3>Organizer Details:</h3>
-            <label htmlFor="organizerName">Organizer Name:</label>
-            <input
-              type="text"
-              id="organizerName"
-              name="organizerName"
-              placeholder="Enter Organizer Name"
-            />
-
-            <label htmlFor="organizerContact">Organizer Contact:</label>
-            <input
-              type="text"
-              id="organizerContact"
-              name="organizerContact"
-              placeholder="Enter Organizer Contact"
-            />
-          </div>
-        )
-      case 3:
-        return (
-          <div className="section">
-            <h3>Date & Time:</h3>
-            <label htmlFor="eventDate">Event Date:</label>
-            <input type="date" id="eventDate" name="eventDate" />
-
-            <label htmlFor="startTime">Start Time:</label>
-            <input
-              type="time"
-              id="startTime"
-              name="startTime"
-              placeholder="Start Time"
-            />
-
-            <label htmlFor="endTime">End Time:</label>
-            <input
-              type="time"
-              id="endTime"
-              name="endTime"
-              placeholder="End Time"
-            />
-          </div>
-        )
-      case 4:
-        return (
-          <div className="section">
-            <h3>Location:</h3>
-            <label htmlFor="eventType">Event Type:</label>
-            <input
-              type="text"
-              id="eventType"
-              name="eventType"
-              placeholder="Select Event Type"
-            />
-
-            <label htmlFor="venueName">Venue Name:</label>
-            <input
-              type="text"
-              id="venueName"
-              name="venueName"
-              placeholder="Enter Venue Name"
-            />
-
-            <label htmlFor="eventAddress">Event Address:</label>
-            <input
-              type="text"
-              id="eventAddress"
-              name="eventAddress"
-              placeholder="Enter Event Address"
-            />
-
-            <label htmlFor="googleMapsLink">Google Maps Link:</label>
-            <input
-              type="url"
-              id="googleMapsLink"
-              name="googleMapsLink"
-              placeholder="Google Maps Link"
-            />
-
-            <label htmlFor="meetingLink">Meeting Link:</label>
-            <input
-              type="url"
-              id="meetingLink"
-              name="meetingLink"
-              placeholder="Meeting Link"
-            />
-          </div>
-        )
-      default:
-        return null
-    }
-  }
-
   return (
     <>
-    
-    <div>
-    <h2 className="Add__Event__title">Add Events</h2>
-      <form className="event-form">
-     
-        {renderFormSection()}
+     <h2 className="Add__Event__title-add-event">Add Events</h2>
+      <div className="Add__container">
+       
+       
+          <div className="form-section-add-event">
+            <div className="column1-add-event">
+              <table className="table-add-event">
+                <tr>
+                  <th colSpan="2">Basic Details:</th>
+                </tr>
 
-        <div className="buttons">
-          {step > 1 && (
-            <button type="button" onClick={prevStep}>
-              Previous
-            </button>
-          )}
-          {step < 4 && (
-            <button type="button" onClick={nextStep}>
-              Next
-            </button>
-          )}
-          {step === 4 && <button type="submit">Submit</button>}
-        </div>
-      </form>
-    </div>
+                <tr>
+                  <td>
+                    <label>Event Name:</label>
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      id="eventName"
+                      name="eventName"
+                      placeholder="Enter Event Name"
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Event Description:</label>
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      id="eventDescription"
+                      name="eventDescription"
+                      placeholder="Enter Description"
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Event Category:</label>
+                  </td>
+                  <td>
+                    <select id="eventCategory" name="eventCategory">
+                      <option>Select Event Category</option>
+                      {/* Add options here */}
+                    </select>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Event Tags:</label>
+                  </td>
+                  <td>
+                    <select id="eventTags" name="eventTags">
+                      <option>Select Event Tags</option>
+                      {/* Add options here */}
+                    </select>
+                  </td>
+                </tr>
+
+                <tr>
+                  <th colSpan="2">Date & Time:</th>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Event Date:</label>
+                  </td>
+                  <td>
+                    <input type="date" id="eventDate" name="eventDate" />
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Start Time:</label>
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      id="startTime"
+                      name="startTime"
+                      placeholder="Enter Time"
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>End Time:</label>
+                  </td>
+                  <td>
+                    <select id="endTime" name="endTime">
+                      <option>Select Event Category</option>
+                      {/* Add options here */}
+                    </select>
+                  </td>
+                </tr>
+              </table>
+            </div>
+
+            <div className="column2-add-event">
+              <table className="table-add-event">
+                <tr>
+                  <th colSpan="2">Location:</th>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Event Type:</label>
+                  </td>
+                  <td>
+                    <select id="eventType" name="eventType">
+                      <option>Select Event Type</option>
+                      {/* Add options here */}
+                    </select>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Venue Name:</label>
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      id="venueName"
+                      name="venueName"
+                      placeholder="Enter Venue Name"
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Event Address:</label>
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      id="eventAddress"
+                      name="eventAddress"
+                      placeholder="Enter Event Address"
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Google Maps Link:</label>
+                  </td>
+                  <td>
+                    <input
+                      type="url"
+                      id="googleMapsLink"
+                      name="googleMapsLink"
+                      placeholder="Google Maps Link"
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Meeting Link:</label>
+                  </td>
+                  <td>
+                    <input
+                      type="url"
+                      id="meetingLink"
+                      name="meetingLink"
+                      placeholder="Meeting Link"
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <th colSpan="2">Organizer Details:</th>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Organizer Name:</label>
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      id="organizerName"
+                      name="organizerName"
+                      placeholder="Enter Organizer Name"
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <label>Organizer Contact:</label>
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      id="organizerContact"
+                      name="organizerContact"
+                      placeholder="Enter Organizer Contact"
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <td colSpan="2">
+                    <button type="submit" className="add_event_button">Submit</button>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+      
+      </div>
     </>
-  )
+  );
 }
